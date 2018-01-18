245a16e91e669265fcbcc9a6f0154c8948875f6c
Jihun Kang <j_kang9@u.pacific.edu>
2018-01-18 15:40:48 -0800

    Initial Commit

---
+++ Find My Area/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Find My Area
+//
+//  Created by Jihun Kang on 1/18/18.
+//  Copyright © 2018 Jihun Kang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
