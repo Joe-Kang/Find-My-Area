21bb6e2d842a1a5cc3c059bb38d4de2728d9509f
Jihun Kang <j_kang9@u.pacific.edu>
2018-01-18 20:13:06 -0800

    Map is Working!

---
+++ Find My Area/MapViewController.swift
@@ -0,0 +1,97 @@
+import UIKit
+import GoogleMaps
+import CoreLocation
+
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        enableBasicLocationServices()
+    }
+    
+    func enableBasicLocationServices() {
+        locationManager.delegate = self
+        
+        switch CLLocationManager.authorizationStatus() {
+        case .notDetermined:
+            // Request when-in-use authorization initially
+            locationManager.requestWhenInUseAuthorization()
+            break
+            
+        case .restricted, .denied:
+            // Disable location features
+            disableMyLocationBasedFeatures()
+            break
+            
+        case .authorizedWhenInUse, .authorizedAlways:
+            // Enable location features
+            enableMyWhenInUseFeatures()
+            break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .restricted, .denied:
+            disableMyLocationBasedFeatures()
+            break
+            
+        case .authorizedWhenInUse:
+            enableMyWhenInUseFeatures()
+            break
+            
+        case .notDetermined, .authorizedAlways:
+            break
+        }
+    }
+    
+    func disableMyLocationBasedFeatures(){
+        print("disableMyLocationBasedFeatures")
+    }
+    
+    func enableMyWhenInUseFeatures(){
+        print("enableMyWhenInUseFeaatures")
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.distanceFilter = 100.0  // In meters.
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager,  didUpdateLocations locations: [CLLocation]) {
+        let lastLocation = locations.last!
+        print(lastLocation)
+        
+        // Do something with the location.
+    }
+    
+    override func loadView() {
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        struct State {
+            let name: String
+            let long: CLLocationDegrees
+            let lat: CLLocationDegrees
+        }
+        
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        let states = [
+            State(name: "Alaska", long: -152.404419, lat: 61.370716),
+            State(name: "Alabama", long: -86.791130, lat: 32.806671),
+            // the other 51 states here...
+        ]
+        // Creates a marker in the center of the map.
+        for state in states {
+            let marker = GMSMarker()
+            marker.position = CLLocationCoordinate2D(latitude: state.lat, longitude: state.long)
+            marker.title = state.name
+            marker.snippet = "Put pin here"
+            marker.map = mapView
+        }
+       
+    }
+}
