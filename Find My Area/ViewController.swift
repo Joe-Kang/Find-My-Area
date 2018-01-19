fc43b226a52492fd80ef1d32ac2e2ff1f4ccc27a
seymakara <seymakara88@gmail.com>
2018-01-19 13:45:07 -0800

    adding to table view

--- Find My Area/ViewController.swift
+++ Find My Area/ViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 import CoreLocation
+import CoreData
 
-class ViewController: UIViewController, MapViewControllerDelegate {
+class ViewController: UIViewController, MapViewControllerDelegate, UITableViewDelegate, UITableViewDataSource {
     
     @IBAction func addMapCoordinates(_ sender: UIBarButtonItem) {
     }
@@ -17,25 +18,103 @@ class ViewController: UIViewController, MapViewControllerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        distanceTableView.dataSource = self
+        distanceTableView.delegate = self
+        
+        areaTableView.dataSource = self
+        areaTableView.delegate = self
+        print("Hello1")
     }
     
-    override func didReceiveMemoryWarning() {super.didReceiveMemoryWarning()}
+    @IBOutlet weak var distanceTableView: UITableView!
+    @IBOutlet weak var areaTableView: UITableView!
+    
+    var distanceItems = ["living room", "garden", "fences"]
+    var areaItems = ["living room", "garden", "fences"]
+    
+    //    FOR DATABASE
+    //    var areas = [AreaItem]()
+    //    var distances = [DistanceItem]()
+    
+    //    let managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        var count = 0
+        
+        if tableView == self.distanceTableView {
+            count = distanceItems.count
+            print("DISTANCE ITEMS\(distanceItems.count)")
+        }
+        if tableView == self.areaTableView {
+            count = areaItems.count
+        }
+        
+        return count
+    }
+    
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var cell = UITableViewCell()
+        
+        if tableView == self.distanceTableView {
+            print("Hello2")
+            cell = tableView.dequeueReusableCell(withIdentifier: "distanceCell", for: indexPath)
+            cell.textLabel?.text = distanceItems[indexPath.row]
+        }
+        if tableView == self.areaTableView {
+            print("Hello3")
+            cell = tableView.dequeueReusableCell(withIdentifier: "areaCell", for: indexPath)
+            cell.textLabel?.text = areaItems[indexPath.row]
+        }
+        return cell
+    }
+    
+   
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let navigationController = segue.destination as! UINavigationController
         let mapViewController = navigationController.topViewController as! MapViewController
         mapViewController.delegate = self
     }
-
+    
     func returnArea(by Controller: MapViewController, _ area: Double) {
         dismiss(animated: true, completion: nil)
         print(area)
-
+        
     }
-
+    
     func returnDistance(by Controller: MapViewController, _ distance: Double) {
         dismiss(animated: true, completion: nil)
         print(distance)
     }
+    
+    
+    //    FOR DATABASE
+    //    func fetchAllItems(){
+    //        let areaRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "AreaItem")
+    //        do {
+    //            let areaResult = try managedObjectContext.fetch(areaRequest)
+    //            let areaitems = areaResult as! [AreaItem]
+    //        } catch {
+    //            print("\(error)")
+    //        }
+    //
+    //        let distanceRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "DistanceItem")
+    //        do {
+    //            let distanceResult = try managedObjectContext.fetch(distanceRequest)
+    //            let distanceitems = distanceResult as! [DistanceItem]
+    //        } catch {
+    //            print("\(error)")
+    //        }
+    //
+    //    }
+    
+    
 }
+
+
+
+
 
